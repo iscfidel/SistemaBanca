8d2b684061a5fb3f119b322a9080f2143d2a9642
iscfidel <gaalex1980@gmail.com>
2025-07-20 19:07:40 -0600

    Update: Creacion de modelo MVC para mejor control.

--- src/main/java/com/fagr/App.java
+++ src/main/java/com/fagr/App.java
@@ -1,32 +1,43 @@
 package com.fagr;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-/**
- * 5. Sistema de Banca (Cuentas y Transacciones)
- * 
- * Descripción:
- * Un sistema bancario que maneje cuentas, depósitos, retiros y transferencias.
- * 
- * Clases principales:
- * 
- * CuentaBancaria (número de cuenta, saldo, titular).
- * 
- * Transaccion (tipo [depósito/retiro/transferencia], monto, fecha).
- * 
- * Banco (lista de cuentas, métodos para realizar transacciones).
- * 
- * Funcionalidades:
- * 
- * Crear cuentas (ahorro/corriente).
- * 
- * Historial de transacciones por cuenta.
- * 
- * Validar fondos antes de retiros.
- * 
- * Exportar movimientos a PDF (usando librerías como Apache PDFBox).
- *
- */
+import com.mysql.jdbc.Statement;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Obtener la instancia Singleton
+        DatabaseConnection dbConnection = DatabaseConnection.getInstance();
+
+        // Obtener la conexión
+        Connection connection = dbConnection.getConnection();
+
+        // Llamar al método para mostrar usuarios
+        ejecutarSelect(connection, "SELECT * FROM usuarios");
+
+        // Cerrar la conexión
+        dbConnection.closeConnection();
     }
+
+    public static void ejecutarSelect(Connection connection, String query) {
+        try (Statement statement = (Statement) connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query)) {
+
+            int columnCount = resultSet.getMetaData().getColumnCount();
+            while (resultSet.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = resultSet.getMetaData().getColumnName(i);
+                    String value = resultSet.getString(i);
+                    System.out.print(columnName + ": " + value + " ");
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Ejemplo de uso en el main:
+    // ejecutarSelect(connection, "SELECT * FROM usuarios");
 }
--- src/main/java/com/fagr/entity/User.java
+++ src/main/java/com/fagr/entity/User.java
@@ -1,4 +1,4 @@
-package com.fagr;
+package com.fagr.entity;
 
 public class User {
     private String name;
